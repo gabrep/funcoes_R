e64398462c0872a7bd2189b3b6007fca59683015
gabrep <pereira.gabriel1@hotmail.com>
2025-09-24 16:25:02 -0300

    adicionado conversão de resultado DESeq para dataframe com nome de colunas igual ao limma e alterado para nao haver opção de definir a coluna de logfc

--- run_GSEA.R
+++ run_GSEA.R
@@ -13,9 +13,24 @@
 
 run_GSEA <- function(res, #dataframe de resultado de DEGs extraído com limma::topTable
                      genes = 'SYMBOL', #coluna dos resultados que traz o identificador dos genes
-                     logfc = 'logFC', #coluna dos resultados que traz o valor de logFC
                      bases = c('HALLMARK', 'KEGG', 'REACTOME', 'WIKIPATHWAYS') #selecionar as bases a se utilizar para rodar o GSEA
 ){
+  #Converter nomes de colunas de resultados vindos de DESEq para manter padrao igual ao limma
+  if (class(res) == 'DESeqResults') {
+    res <- as.data.frame(res)
+    res$SYMBOL <- rownames(res) 
+    
+    if ('log2FoldChange' %in% colnames(res)) {
+      res <- dplyr::rename(res, logFC = log2FoldChange)
+    }
+    
+    if ('padj' %in% colnames(res)) {
+      res <- dplyr::rename(res, adj.P.Val = padj)
+    }
+   
+  }
+  
+  
   ##Download das informações para enriquecimento, de acordo com as bases selecionadas
   
   CPs <- NULL
@@ -36,19 +51,19 @@ run_GSEA <- function(res, #dataframe de resultado de DEGs extraído com limma::t
   
     #remover os genes duplicados, mantendo apenas os de valores mais extremos (maior valor para logFC positivo, menor para logFC negativo)
     
-    res <- res[order(abs(res[[logfc]]), decreasing=T),]
+    res <- res[order(abs(res$logFC), decreasing=T),]
     res <- res[!duplicated(res[[genes]]),] %>% na.omit()
     
     
   ### Criar rank de valores de expressão ##
-    ord <- order(res[[logfc]], decreasing = T) #ordena os genes do maior logFC para o menor
+    ord <- order(res$logFC, decreasing = T) #ordena os genes do maior logFC para o menor
     res_ord <- res[ord,] #dataframe de resultados com a ordem estabelecida
     
     # Remover NAs
     res_ord <- res_ord[!is.na(res_ord[[genes]]),] #remove os genes sem simbolo mantendo a ordem
     
     #criar o vetor de ranks
-    ranks <- res_ord[[logfc]]
+    ranks <- res_ord$logFC
     names(ranks) <- res_ord[[genes]]
     
 
